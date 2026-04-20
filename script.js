906cd5da1030403e3d862b1df02ffcc6bda06818
wtspcrvtt <lenya.stepanov.07@gmail.com>
2026-04-21 01:11:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const loadBtn = document.getElementById('loadBtn');
+
+function loadProducts() {
+    fetch('https://zalupa.kaizo.wtf/api/v1/products/')
+        .then(Response => Response.json())
+        .then(data => {
+            console.log(data);
+            const container = document.getElementById('container-products')
+            container.innerHTML = ''
+            for (let i = 0; i<data.products.length; i++) {
+                const product = data.products[i];
+                const card = document.createElement('div');
+                card.className = 'product-card';
+                card.textContent = product.title;
+                container.appendChild(card);
+                const img = document.createElement('img');
+                img.className = 'product-img';
+                img.src = product.image_url;
+                container.appendChild(img);
+            };
+        })
+        .catch(error => console.error('Ошибка:', error))
+};
+
+
+
+
+loadBtn.addEventListener('click', loadProducts);
