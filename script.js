56facdb5c82c0048b7762b52d7cbee8706d04aaf
wtspcrvtt <lenya.stepanov.07@gmail.com>
2026-04-23 01:44:12 +0300

    add products detail card

--- script.js
+++ script.js
@@ -23,10 +23,37 @@ function loadProducts() {
                 card.appendChild(img);
                 card.appendChild(price);
                 container.appendChild(card);
+                card.dataset.id = product.id;
+                card.addEventListener('click', () => {
+                    loadProductsDetail(product.id);
+                });
             };
         })
         .catch(error => console.error('Ошибка:', error))
 };
 
+function loadProductsDetail(id) {
+    fetch(`https://zalupa.kaizo.wtf/api/v1/products/${id}`)
+        .then(Response => Response.json())
+        .then(data => {
+            console.log(data);
+            const productName = document.getElementById('product-name');
+            const productImg = document.getElementById('product-img');
+            const productPrice = document.getElementById('product-price');
+            const productInf = document.getElementById('product-info');
+            productName.textContent = data.title;
+            productImg.src = data.image_url;
+            productPrice.textContent = data.price;
+            productInf.textContent = data.description;
+            document.getElementById('container-products').style.display = 'none';
+            document.getElementById('product-detail').style.display = 'block';
+        })
+};
 
-document.addEventListener('DOMContentLoaded', loadProducts());
+const backBtn = document.getElementById('back-btn');
+
+document.addEventListener('DOMContentLoaded', loadProducts);
+backBtn.addEventListener('click', () => {
+    document.getElementById('container-products').style.display = 'block';
+    document.getElementById('product-detail').style.display = 'none';
+});
